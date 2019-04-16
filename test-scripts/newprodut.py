f191208309a94898a1e5bfed524ebe1c0a0aa4f3
SyS <sreyassreelal@gmail.com>
2019-04-16 06:31:17 +0000

    test product add

---
+++ test-scripts/newprodut.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+re = requests.post(
+    "http://localhost:8000/product/new",
+    data=json.dumps({
+        'product_name': "Helmet",
+        'price': 300,
+        'rating': 7.0,
+    })
+    )
+print("Response: ",re.content)
+
+session = requests.Session()
+re = session.post(
+    "http://localhost:8000/user/auth",
+    data=json.dumps({
+        'name':'Sreyas',
+        'password':'mypass'
+}))
+
+print("Response: ",re.content)
+#print("\n Session cookies ",re.cookies)
+token = re.cookies.get("jwt")
+print("Token is ",token)
+
+re = session.post(
+    "http://localhost:8000/product/new",
+    data=json.dumps({
+        'product_name': "Helmet",
+        'price': 300,
+        'rating': 7.0,
+    }),
+    cookies={"jwt":token}
+    )
+print("Response: ",re.content)
+
